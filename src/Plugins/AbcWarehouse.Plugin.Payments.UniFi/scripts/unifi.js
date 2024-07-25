59d4b5a043f2007934718692e9d87d3bb8730424
Dave Farinelli <d@dfar.io>
2024-07-25 18:10:25 +0000

    Add Unifi event data debugging

--- src/Plugins/AbcWarehouse.Plugin.Payments.UniFi/scripts/unifi.js
+++ src/Plugins/AbcWarehouse.Plugin.Payments.UniFi/scripts/unifi.js
@@ -4,6 +4,8 @@ $(document).ready(function () {
 	$('.payment-info-next-step-button').hide();
 
     window.addEventListener("message",function(event) {
+		console.log(`event data type is: ${typeof event.data}`);
+		console.log(`event data is: ${event.data}`);
 		if ((typeof event.data == 'string' || typeof event.data == 'object') && (event.data == 'Close Model' || event.data == 'Return To Merchant Shipping' || event.data == 'Close' || event.data.action == 'setPayCloseModal' || event.data.event == 'return-to-partner')) {
 				var transactionToken = document.getElementById('transactionToken').value;
 				$.ajax({
