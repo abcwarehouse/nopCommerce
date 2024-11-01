0703362927c92ff00b0752dcae9018b108fe3724
Dave Farinelli <d@dfar.io>
2024-11-01 17:29:35 +0000

    Add connection to staging DB

--- src/Plugins/Nop.Plugin.Misc.AbcCore/Tasks/UpdatePdpVideosTask.cs
+++ src/Plugins/Nop.Plugin.Misc.AbcCore/Tasks/UpdatePdpVideosTask.cs
@@ -4,21 +4,34 @@ using Nop.Services.Catalog;
 using Nop.Services.Tasks;
 using Nop.Data;
 using Nop.Services.Logging;
+using Microsoft.Data.SqlClient;
 
 namespace Nop.Plugin.Misc.AbcCore.Tasks
 {
     class UpdatePdpVideosTask : IScheduleTask
     {
+        private readonly CoreSettings _settings;
         private readonly ILogger _logger;
 
-        public UpdatePdpVideosTask(ILogger logger)
+        public UpdatePdpVideosTask(
+            CoreSettings settings,
+            ILogger logger)
         {
+            _settings = settings;
             _logger = logger;
         }
 
         public async System.Threading.Tasks.Task ExecuteAsync()
         {
             await _logger.InformationAsync("Updating PDP videos.");
+
+            using (SqlConnection connection = new SqlConnection(_settings.StagingDbConnectionString))
+            {
+                var queryString = "SELECT * FROM rws_product_video";
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Connection.Open();
+                command.ExecuteNonQuery();
+            }
 
             await _logger.InformationAsync("PDP videos updated.");
         }
