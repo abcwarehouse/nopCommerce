8f206e8ea441d63c2de3955dddf938869caf3bde
imperfectsworld <imperfectsworld@gmail.com>
2025-01-16 13:25:55 -0500

    implemented a method to get the brand name from the brand id number

--- src/Plugins/Nop.Plugin.Widgets.AbcPromos/Tasks/LegacyTasks/GenerateRebatePromoPageTask.cs
+++ src/Plugins/Nop.Plugin.Widgets.AbcPromos/Tasks/LegacyTasks/GenerateRebatePromoPageTask.cs
@@ -111,12 +111,14 @@ namespace Nop.Plugin.Widgets.AbcPromos.Tasks.LegacyTasks
                 var promoDescription = promo.ManufacturerId != null ?
                                 $"{(await _manufacturerService.GetManufacturerByIdAsync(promo.ManufacturerId.Value)).Name} - {promo.Description}" :
                                 promo.Description;
-                html += $"<div class=\"abc-item abc-promo-item\"> " + $"<h1>{promo.ManufacturerId}</h1>" +
+                html += $"<div class=\"abc-item abc-promo-item\"> " + $"<h1>{GetProductManufacturersByProductIdAsync(promo.ManufacturerId)}</h1>" + 
                         $"<a href=\"/promos/{await _urlRecordService.GetActiveSlugAsync(promo.Id, "AbcPromo", 0)}\"> " +
-                        $"{promoDescription} Hello I am alive</a><br />" +
+                        $"{promoDescription}</a><br />" +
                         $"Expires {promo.EndDate.ToString("MM-dd-yy")}" + 
 
                         "</div>";
+
+                         
             }
 
             html += "</div>";
