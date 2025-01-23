1c54ebe84afb86f230bcc22daf86f6de02c15d4a
“Mike0081” <mikeschwartz81@gmail.com>
2025-01-23 14:53:19 -0500

    Move api credentials

--- src/Libraries/Nop.Services/custom/ListrakApiService.cs
+++ src/Libraries/Nop.Services/custom/ListrakApiService.cs
@@ -3,71 +3,27 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
-using System.Text;
-using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 using Nop.Core.Domain.Common;
-using Nop.Core.Domain.Customers;
 using Nop.Services.Custom;
-using JsonSerializer = System.Text.Json.JsonSerializer;
 
 public class ListrakApiService : IListrakApiService
 {
     private readonly HttpClient _httpClient;
     private readonly IHttpClientFactory _httpClientFactory;
     private readonly IConfiguration _configuration;
+    private readonly ListrakApiSettings _listrakApiSettings;
 
-    public ListrakApiService(HttpClient httpClient, IHttpClientFactory httpClientFactory, IConfiguration configuration)
+    public ListrakApiService(HttpClient httpClient, IHttpClientFactory httpClientFactory, IConfiguration configuration, ListrakApiSettings listrakApiSettings)
     {
         _httpClientFactory = httpClientFactory;
         _httpClient = httpClient;
         _configuration = configuration;
         _httpClient.BaseAddress = new Uri("https://api.listrak.com");
+        _listrakApiSettings = listrakApiSettings;
     }
-
-    /*public string GetToken()
-    {
-        var tokenEndpoint = "/OAuth2/Token"; // Relative path for token API
-        var credentials = new
-        {
-            Username = "ao1xkc57sz7t1dw1qawh",
-            Password = "rDpBSv2PMMrpo2Nso0AAyFqiag1U395bYV4ltx1vhIE"
-        };
-
-        try
-        {
-            var response = _httpClient.PostAsJsonAsync(tokenEndpoint, credentials).Result;
-
-            if (response.IsSuccessStatusCode)
-            {
-                var content = response.Content.ReadAsStringAsync().Result;
-
-                var tokenResponse = JsonSerializer.Deserialize<TokenResponse>(content, new JsonSerializerOptions
-                {
-                    PropertyNameCaseInsensitive = true
-                });
-
-                if (tokenResponse != null && !string.IsNullOrEmpty(tokenResponse.AccessToken))
-                {
-                    return tokenResponse.AccessToken;
-                }
-                throw new Exception("Token is null or empty in the API response.");
-            }
-            else
-            {
-                // Log or handle API failure
-                throw new Exception($"API request failed with status code {response.StatusCode} and reason: {response.ReasonPhrase}");
-            }
-        }
-        catch (Exception ex)
-        {
-            // Log the detailed exception for troubleshooting
-            Console.WriteLine($"Exception in GetToken: {ex.Message}");
-            throw new Exception("Failed to retrieve the API token.", ex);
-        }
-    }*/ 
 
     public async Task<string> GetTokenAsync()
     {
@@ -77,7 +33,7 @@ public class ListrakApiService : IListrakApiService
             {
                 Content = new FormUrlEncodedContent(new Dictionary<string, string>
                 {
-                    { "client_id", "ao1xkc57sz7t1dw1qawh" },
+                    { "client_id", _listrakApiSettings.ClientId },
                     { "client_secret", "rDpBSv2PMMrpo2Nso0AAyFqiag1U395bYV4ltx1vhIE" },
                     { "grant_type", "client_credentials" }
                 })
