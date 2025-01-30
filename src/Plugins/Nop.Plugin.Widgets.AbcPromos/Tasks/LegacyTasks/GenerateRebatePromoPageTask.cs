379e36facdbfcb79e409f2d740be4031000de955
imperfectsworld <imperfectsworld@gmail.com>
2025-01-30 12:57:51 -0500

    finally fixed the manButton

--- src/Plugins/Nop.Plugin.Widgets.AbcPromos/Tasks/LegacyTasks/GenerateRebatePromoPageTask.cs
+++ src/Plugins/Nop.Plugin.Widgets.AbcPromos/Tasks/LegacyTasks/GenerateRebatePromoPageTask.cs
@@ -125,7 +125,7 @@ namespace Nop.Plugin.Widgets.AbcPromos.Tasks.LegacyTasks
     foreach (var group in promoGroups.OrderBy(g => g.Key))
     {
         string manName = group.Key;
-        string promoSlug1 = await _urlRecordService.GetActiveSlugAsync(group.Value.First().Id, "AbcPromo", 0) ?? "default-slug";
+        
 
 
         html += $"<h1>{manName}</h1>";
@@ -141,7 +141,7 @@ namespace Nop.Plugin.Widgets.AbcPromos.Tasks.LegacyTasks
                    "</div>";
         }
 
-        html += $"<a class=\"ManButton\" href=\"/promos/{promoSlug1}\">Shop {manName}</a>";
+        html += $"<a class=\"ManButton\" href=\"/promos/{manName}\">Shop {manName}</a>";
     }
 
     return html;
