9baa7888348f01afda24fc2d0be8b500d07efce9
imperfectsworld <imperfectsworld@gmail.com>
2025-02-10 09:40:27 -0500

    added the missing equal sign in class = "manname" and made minor adjustments to the css

--- src/Plugins/Nop.Plugin.Widgets.AbcPromos/Tasks/LegacyTasks/GenerateRebatePromoPageTask.cs
+++ src/Plugins/Nop.Plugin.Widgets.AbcPromos/Tasks/LegacyTasks/GenerateRebatePromoPageTask.cs
@@ -129,7 +129,7 @@ namespace Nop.Plugin.Widgets.AbcPromos.Tasks.LegacyTasks
 
 
 
-                html += $"<h1 class\"manName\">{manName}</h1>";
+                html += $"<h1 class=\"manName\">{manName}</h1>";
 
                 foreach (var promo in group.Value)
                 {
