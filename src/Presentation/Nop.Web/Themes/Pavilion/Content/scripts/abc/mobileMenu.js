783cedf237cd58e52ef94edefbe256c08130d700
imperfectsworld <imperfectsworld@gmail.com>
2025-04-15 17:00:24 -0400

    added in the hawthorne rebates pic and hopefully also fixed the shop all in hawthorne

--- src/Presentation/Nop.Web/Themes/Pavilion/Content/scripts/abc/mobileMenu.js
+++ src/Presentation/Nop.Web/Themes/Pavilion/Content/scripts/abc/mobileMenu.js
@@ -52,7 +52,7 @@ $(document).ready(function () {
          // Special case for "shop-all-categories"
     if (categoryName.toLowerCase() === "shop all categories") {
 
-        if(isHawthorne)
+        if(isHawthorne == false)
         {
             selectCategory.attr("href", baseURL1 + "filterSearch");
         }
