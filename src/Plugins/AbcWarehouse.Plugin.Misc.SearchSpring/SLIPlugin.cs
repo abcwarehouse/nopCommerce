97b19a02924c47c202f4fb361ec09822f66644f1
“Mike0081” <mikeschwartz81@gmail.com>
2025-05-12 15:40:09 -0400

    Temp store

---
+++ src/Plugins/AbcWarehouse.Plugin.Misc.SearchSpring/SLIPlugin.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Nop.Services.Common;
+using Nop.Services.Configuration;
+using Nop.Services.Localization;
+using Nop.Services.Plugins;
+using Nop.Web.Framework.Infrastructure;
+
+namespace AbcWarehouse.Plugin.Misc.SearchSpring
+{
+    public class SearchSpringPlugin : BasePlugin, IMiscPlugin
+    {
+    }
+}
