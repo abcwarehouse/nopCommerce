2e5f04fa50805a4eb68c42eb2be4c11e299a4939
“Mike0081” <mikeschwartz81@gmail.com>
2025-05-15 12:25:48 -0400

    Update for errors

--- src/Plugins/AbcWarehouse.Plugin.Misc.SearchSpring/SearchSpring.cs
+++ src/Plugins/AbcWarehouse.Plugin.Misc.SearchSpring/SearchSpring.cs
@@ -4,26 +4,9 @@ using Nop.Web.Framework.Infrastructure;
 using Nop.Web.Framework.Menu;
 using Nop.Web.Framework.UI;
 using Nop.Services.Cms;
+using System.Collections.Generic;
 
-public class MyHeaderInfoWidget : BasePlugin, IWidgetPlugin
+public class MyHeaderInfoWidget : BasePlugin
 {
-    public IList<string> GetWidgetZones()
-    {
-        return new List<string> { PublicWidgetZones.HeaderLinksAfter };
-    }
-
-    public string GetWidgetViewComponentName(string widgetZone)
-    {
-        return "SearchSpring";
-    }
-
-    public override void Install()
-    {
-        base.Install();
-    }
-
-    public override void Uninstall()
-    {
-        base.Uninstall();
-    }
+    
 }
