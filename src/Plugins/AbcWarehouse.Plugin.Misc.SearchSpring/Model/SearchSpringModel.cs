597d57aba7b17f73e4bbbbb9aee5e0960a90a7f1
“Mike0081” <mikeschwartz81@gmail.com>
2025-05-20 11:13:56 -0400

    Add model

---
+++ src/Plugins/AbcWarehouse.Plugin.Misc.SearchSpring/Model/SearchSpringModel.cs
@@ -0,0 +1,10 @@
+using Nop.Web.Framework.Models;
+using Nop.Web.Framework.Mvc.ModelBinding;
+
+namespace AbcWarehouse.Plugin.Misc.SearchSpring.Models
+{
+    public record SearchSpringModel : BaseNopModel
+    {
+
+    }
+}
