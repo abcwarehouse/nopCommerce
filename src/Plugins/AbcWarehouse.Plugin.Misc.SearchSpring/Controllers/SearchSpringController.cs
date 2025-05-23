22b4bb965f5d7f3c0ea0180216999d9feeed2223
“Mike0081” <mikeschwartz81@gmail.com>
2025-05-23 13:37:48 -0400

    More logging

--- src/Plugins/AbcWarehouse.Plugin.Misc.SearchSpring/Controllers/SearchSpringController.cs
+++ src/Plugins/AbcWarehouse.Plugin.Misc.SearchSpring/Controllers/SearchSpringController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using AbcWarehouse.Plugin.Misc.SearchSpring.Services;
+using System;
 
 namespace AbcWarehouse.Plugin.Misc.SearchSpring.Controllers
 {
@@ -23,6 +24,11 @@ namespace AbcWarehouse.Plugin.Misc.SearchSpring.Controllers
         [HttpGet]
         public async Task<IActionResult> Results(string term)
         {
+            Console.WriteLine($"[Controller] Received term: {term}");
+
+            if (string.IsNullOrWhiteSpace(term))
+                return BadRequest("Search term cannot be empty.");
+
             var results = await _searchSpringService.SearchAsync(term);
             return View("~/Plugins/AbcWarehouse.Plugin.Misc.SearchSpring/Views/Results.cshtml", results);
         }
