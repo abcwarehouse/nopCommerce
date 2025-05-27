ced5c61610c8ef7559ceab47a255ece20e7621bd
“Mike0081” <mikeschwartz81@gmail.com>
2025-05-27 10:45:13 -0400

    More updates

--- src/Plugins/AbcWarehouse.Plugin.Misc.SearchSpring/Controllers/SearchSpringController.cs
+++ src/Plugins/AbcWarehouse.Plugin.Misc.SearchSpring/Controllers/SearchSpringController.cs
@@ -29,5 +29,27 @@ namespace AbcWarehouse.Plugin.Misc.SearchSpring.Controllers
             var results = await _searchSpringService.SearchAsync(q);
             return View("~/Plugins/AbcWarehouse.Plugin.Misc.SearchSpring/Views/Results.cshtml", results);
         }
+
+        [Route("searchspring/suggest")]
+        [HttpGet]
+        public async Task<IActionResult> Suggest(string q)
+        {
+            if (string.IsNullOrWhiteSpace(q))
+                return BadRequest("Query is required");
+
+            var client = _httpClientFactory.CreateClient();
+            var suggestUrl = $"https://4lt84w.a.searchspring.io/api/suggest/search?q={WebUtility.UrlEncode(q)}";
+
+            var response = await client.GetAsync(suggestUrl);
+            if (!response.IsSuccessStatusCode)
+            {
+                var error = await response.Content.ReadAsStringAsync();
+                return BadRequest($"Searchspring Suggest API error: {error}");
+            }
+
+            var content = await response.Content.ReadAsStringAsync();
+            return Content(content, "application/json");
+        }
+
     }
 }
