e81d3bb41ddea34d92ae1bb56dacbeb54947ad49
“Mike0081” <mikeschwartz81@gmail.com>
2025-06-10 11:29:10 -0400

    Additional Changes to MikeSpring

--- src/Plugins/AbcWarehouse.Plugin.Misc.SearchSpring/Model/SearchResultsModel.cs
+++ src/Plugins/AbcWarehouse.Plugin.Misc.SearchSpring/Model/SearchResultsModel.cs
@@ -14,6 +14,33 @@ namespace AbcWarehouse.Plugin.Misc.SearchSpring.Models
         public int TotalPages => (int)Math.Ceiling((double)TotalResults / PageSize);
 
         public string Query { get; set; }
+        public Dictionary<string, FacetDetail> Facets { get; set; } = new();
+        public List<SortOption> SortOptions { get; set; } = new();
+    }
+
+    public class FacetDetail
+    {
+        public string Multiple { get; set; }
+        public string Display { get; set; }
+        public string Label { get; set; }
+        public bool Collapsed { get; set; }
+
+        public List<FacetValue> Values { get; set; } = new();
+    }
+
+    public class FacetValue
+    {
+        public string Value { get; set; }
+        public string Label { get; set; }
+        public int Count { get; set; }
+    }
+
+    public class SortOption
+    {
+        public string Type { get; set; }
+        public string Field { get; set; }
+        public string Direction { get; set; }
+        public string Label { get; set; }
     }
 
 }
