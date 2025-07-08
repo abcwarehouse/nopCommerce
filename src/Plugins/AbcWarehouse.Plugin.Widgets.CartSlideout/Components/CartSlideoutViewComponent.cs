5d17c3d6add9a03bc67c645bea9430fae4e9cf1a
imperfectsworld <imperfectsworld@gmail.com>
2025-07-08 11:06:15 -0400

    checks if the item in cart is an appliance, if it is then will display teh originall us appliance text and if its not will tell teh user we cant deliver to that address

--- src/Plugins/AbcWarehouse.Plugin.Widgets.CartSlideout/Components/CartSlideoutViewComponent.cs
+++ src/Plugins/AbcWarehouse.Plugin.Widgets.CartSlideout/Components/CartSlideoutViewComponent.cs
@@ -1,13 +1,55 @@
 using Microsoft.AspNetCore.Mvc;
 using Nop.Web.Framework.Components;
+using Nop.Services.Catalog;
+using Nop.Core.Domain.Catalog;
+using System.Threading.Tasks;
+using Nop.Plugin.Misc.AbcCore.Nop;
+using Nop.Services.ShoppingCart;
 
 namespace AbcWarehouse.Plugin.Widgets.CartSlideout.Components
 {
     public class CartSlideoutViewComponent : NopViewComponent
     {
-        public IViewComponentResult Invoke(string widgetZone, object additionalData = null)
+        private readonly IShoppingCartService _shoppingCartService;
+        private readonly IAbcCategoryService _abcCategoryService;
+        private readonly IProductService _productService;
+        private readonly IWorkContext _workContext;
+
+        public CartSlideoutViewComponent(
+            IShoppingCartService shoppingCartService,
+            IAbcCategoryService abcCategoryService,
+            IProductService productService,
+            IWorkContext workContext)
         {
-            return View("~/Plugins/Widgets.CartSlideout/Views/Slideout.cshtml");
+            _shoppingCartService = shoppingCartService;
+            _abcCategoryService = abcCategoryService;
+            _productService = productService;
+            _workContext = workContext;
+        }
+
+        public async Task<IViewComponentResult> Invoke(string widgetZone, object additionalData = null)
+        {
+            var customer = await _workContext.GetCurrentCustomerAsync();
+            var cart = await _shoppingCartService.GetShoppingCartAsync(customer);
+            
+            bool hasAppliances = false;
+            foreach (var item in cart)
+            {
+                var product = await _productService.GetProductByIdAsync(item.ProductId);
+                var productCategories = await _productService.GetProductCategoriesByProductIdAsync(product.Id);
+                
+                foreach (var pc in productCategories)
+                {
+                    if (await _abcCategoryService.HasApplianceTopLevelCategoryAsync(pc.CategoryId))
+                    {
+                        hasAppliances = true;
+                        break;
+                    }
+                }
+                if (hasAppliances) break;
+            }
+
+            return View("~/Plugins/Widgets.CartSlideout/Views/Slideout.cshtml", hasAppliances);
         }
     }
 }
