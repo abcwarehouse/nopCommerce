9ad506abe013d7b082763ad5a09e20b8e822b96c
“Mike0081” <mikeschwartz81@gmail.com>
2025-07-08 09:54:35 -0400

    Forgot using statement., Oops

--- src/Plugins/AbcWarehouse.Plugin.Misc.SearchSpring/Services/SearchSpringService.cs
+++ src/Plugins/AbcWarehouse.Plugin.Misc.SearchSpring/Services/SearchSpringService.cs
@@ -6,6 +6,7 @@ using System.Text.Json;
 using System.Threading.Tasks;
 using AbcWarehouse.Plugin.Misc.SearchSpring.Models;
 using System.Web;
+using System.Linq;
 
 namespace AbcWarehouse.Plugin.Misc.SearchSpring.Services
 {
