1d5cfd80772b21aa045c05aeeea754f7c63c115f
imperfectsworld <imperfectsworld@gmail.com>
2025-07-08 10:21:26 -0400

    test 1

--- src/Plugins/AbcWarehouse.Plugin.Widgets.CartSlideout/Components/CartSlideoutViewComponent.cs
+++ src/Plugins/AbcWarehouse.Plugin.Widgets.CartSlideout/Components/CartSlideoutViewComponent.cs
@@ -1,13 +1,46 @@
 using Microsoft.AspNetCore.Mvc;
 using Nop.Web.Framework.Components;
+using Nop.Services.Catalog;
+using Nop.Core.Domain.Catalog;
+using System.Threading.Tasks;
+using Nop.Plugin.Misc.AbcCore.Nop;
 
 namespace AbcWarehouse.Plugin.Widgets.CartSlideout.Components
 {
     public class CartSlideoutViewComponent : NopViewComponent
     {
-        public IViewComponentResult Invoke(string widgetZone, object additionalData = null)
+        private readonly IShoppingCartModelFactory _shoppingCartModelFactory;
+        private readonly IAbcCategoryService _abcCategoryService;
+        private readonly IProductService _productService;
+
+        public CartSlideoutViewComponent(
+            IShoppingCartModelFactory shoppingCartModelFactory,
+            IAbcCategoryService abcCategoryService,
+            IProductService productService)
         {
-            return View("~/Plugins/Widgets.CartSlideout/Views/Slideout.cshtml");
+            _shoppingCartModelFactory = shoppingCartModelFactory;
+            _abcCategoryService = abcCategoryService;
+            _productService = productService;
+        }
+
+        public async Task<IViewComponentResult> Invoke(string widgetZone, object additionalData = null)
+        {
+            // Get the current shopping cart
+            var cart = await _shoppingCartModelFactory.PrepareShoppingCartModelAsync();
+            
+            // Check if any items in cart are appliances
+            bool hasAppliances = false;
+            foreach (var item in cart.Items)
+            {
+                var product = await _productService.GetProductByIdAsync(item.ProductId);
+                if (await _abcCategoryService.HasApplianceTopLevelCategoryAsync(product))
+                {
+                    hasAppliances = true;
+                    break;
+                }
+            }
+
+            return View("~/Plugins/Widgets.CartSlideout/Views/Slideout.cshtml", hasAppliances);
         }
     }
 }
