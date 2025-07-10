e4d8488a4c5b7c582bbc2cda3738cf9b72b6046b
imperfectsworld <imperfectsworld@gmail.com>
2025-07-10 11:37:53 -0400

    updated the view component with the logic to determine if teh item is an appliance or not

--- src/Plugins/AbcWarehouse.Plugin.Widgets.CartSlideout/Components/CartSlideoutViewComponent.cs
+++ src/Plugins/AbcWarehouse.Plugin.Widgets.CartSlideout/Components/CartSlideoutViewComponent.cs
@@ -1,13 +1,62 @@
 using Microsoft.AspNetCore.Mvc;
+using Nop.Core.Domain.Catalog;
+using Nop.Services.Catalog;
+using Nop.Services.Orders;
+using Nop.Services.Customers;
+using Nop.Services.Seo;
+using Nop.Services.Stores;
+using Nop.Core;
 using Nop.Web.Framework.Components;
+using System.Linq;
+using System.Threading.Tasks;
+using AbcWarehouse.Plugin.Misc.AbcCore.Nop; // Adjust to match actual namespace
 
 namespace AbcWarehouse.Plugin.Widgets.CartSlideout.Components
 {
     public class CartSlideoutViewComponent : NopViewComponent
     {
-        public IViewComponentResult Invoke(string widgetZone, object additionalData = null)
+        private readonly IAbcCategoryService _abcCategoryService;
+        private readonly IShoppingCartService _shoppingCartService;
+        private readonly IProductService _productService;
+        private readonly IWorkContext _workContext;
+
+        public CartSlideoutViewComponent(
+            IAbcCategoryService abcCategoryService,
+            IShoppingCartService shoppingCartService,
+            IProductService productService,
+            IWorkContext workContext)
         {
-            return View("~/Plugins/Widgets.CartSlideout/Views/Slideout.cshtml");
+            _abcCategoryService = abcCategoryService;
+            _shoppingCartService = shoppingCartService;
+            _productService = productService;
+            _workContext = workContext;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string widgetZone, object additionalData = null)
+        {
+            var customer = await _workContext.GetCurrentCustomerAsync();
+            var cart = await _shoppingCartService.GetShoppingCartAsync(customer, ShoppingCartType.ShoppingCart);
+
+            bool hasAppliance = false;
+
+            foreach (var item in cart)
+            {
+                var productCategories = await _productService.GetProductCategoriesByProductIdAsync(item.ProductId);
+
+                foreach (var productCategory in productCategories)
+                {
+                    if (await _abcCategoryService.HasApplianceTopLevelCategoryAsync(productCategory.CategoryId))
+                    {
+                        hasAppliance = true;
+                        break;
+                    }
+                }
+
+                if (hasAppliance)
+                    break;
+            }
+
+            return View("~/Plugins/Widgets.CartSlideout/Views/Slideout.cshtml", hasAppliance);
         }
     }
 }
