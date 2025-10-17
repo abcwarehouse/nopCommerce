691d4d8f4378c12876105453115e4a4f3b9fb94e
“Mike0081” <mikeschwartz81@gmail.com>
2025-10-17 13:19:38 -0400

    Test update controller

--- src/Plugins/AbcWarehouse.Plugin.Misc.SearchSpring/Controllers/SearchSpringBeaconController.cs
+++ src/Plugins/AbcWarehouse.Plugin.Misc.SearchSpring/Controllers/SearchSpringBeaconController.cs
@@ -1,5 +1,7 @@
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Nop.Services.Logging;
+using System;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +13,6 @@ namespace AbcWarehouse.Plugin.Misc.SearchSpring.Controllers
     {
         private readonly ILogger _logger;
         private readonly HttpClient _httpClient;
-
         private const string BeaconApiUrl = "https://beacon.searchspring.io/api/beacon";
 
         public SearchSpringBeaconController(ILogger logger, IHttpClientFactory httpClientFactory)
@@ -28,7 +29,13 @@ namespace AbcWarehouse.Plugin.Misc.SearchSpring.Controllers
 
             try
             {
+                // Ensure the data is posted to Searchspring exactly as JSON
                 var content = new StringContent(eventData.ToString(), Encoding.UTF8, "application/json");
+
+                // Keep referrer/origin headers intact
+                _httpClient.DefaultRequestHeaders.Referrer = Request.GetTypedHeaders().Referer;
+                _httpClient.DefaultRequestHeaders.Add("Origin", Request.Headers["Origin"].ToString());
+
                 var response = await _httpClient.PostAsync(BeaconApiUrl, content);
                 var responseBody = await response.Content.ReadAsStringAsync();
 
@@ -43,15 +50,16 @@ namespace AbcWarehouse.Plugin.Misc.SearchSpring.Controllers
 
                 return Ok(new { success = true });
             }
-            catch (System.Exception ex)
+            catch (Exception ex)
             {
                 await _logger.InsertLogAsync(
                     Nop.Core.Domain.Logging.LogLevel.Error,
                     "[Searchspring Beacon Error]",
-                    ex.Message
+                    ex.ToString()
                 );
                 return StatusCode(500, ex.Message);
             }
         }
     }
+
 }
