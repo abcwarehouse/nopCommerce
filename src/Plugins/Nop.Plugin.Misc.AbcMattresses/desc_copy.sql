7954a058438c8dc990d579e91d96e9670890d6de
mike0081 <mikeschwartz81@gmail.com>
2025-12-08 16:32:10 -0500

    SQL Update plp des

--- src/Plugins/Nop.Plugin.Misc.AbcMattresses/desc_copy.sql
+++ src/Plugins/Nop.Plugin.Misc.AbcMattresses/desc_copy.sql
@@ -1,18 +1,26 @@
--- Add PLP Description
-INSERT INTO GenericAttribute (EntityId, KeyGroup, [Key], [Value], StoreId, CreatedOrUpdatedDateUTC)
-select hp.Id, 'Product', 'PLPDescription', aga.[Value], 0, GETDATE()
-from NOPCommerce.dbo.GenericAttribute aga
-join NOPCommerce.dbo.Product ap on ap.Id = aga.EntityId
-join Product hp on ap.Sku = hp.Sku
-where KeyGroup = 'Product' and [Key] = 'PLPDescription' and EntityId in (select ProductId from NOPCommerce.dbo.AbcMattressModel)
-
--- Update Short Description
-UPDATE 
-    Product
-SET 
-    ShortDescription = ap.ShortDescription,
-    FullDescription = ap.FullDescription
-FROM 
-    Product hp
-JOIN NOPCommerce.dbo.Product ap ON ap.Sku = hp.Sku
-WHERE hp.Id in (select ProductId from NOPCommerce.dbo.AbcMattressModel)
+MERGE GenericAttribute AS target
+USING (
+    select 
+        hp.Id as EntityId,
+        'Product' as KeyGroup,
+        'PLPDescription' as [Key],
+        aga.[Value] as [Value],
+        0 as StoreId
+    from NOPCommerce.dbo.GenericAttribute aga
+    join NOPCommerce.dbo.Product ap on ap.Id = aga.EntityId
+    join Product hp on ap.Sku = hp.Sku
+    where aga.KeyGroup = 'Product'
+      and aga.[Key] = 'PLPDescription'
+      and aga.EntityId in (select ProductId from NOPCommerce.dbo.AbcMattressModel)
+) AS source
+ON target.EntityId = source.EntityId
+   AND target.KeyGroup = source.KeyGroup
+   AND target.[Key] = source.[Key]
+   AND target.StoreId = source.StoreId
+WHEN MATCHED THEN 
+    UPDATE SET 
+        target.[Value] = source.[Value],
+        target.CreatedOrUpdatedDateUTC = GETDATE()
+WHEN NOT MATCHED THEN
+    INSERT (EntityId, KeyGroup, [Key], [Value], StoreId, CreatedOrUpdatedDateUTC)
+    VALUES (source.EntityId, source.KeyGroup, source.[Key], source.[Value], source.StoreId, GETDATE());
